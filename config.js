dbaa6eaa672932b802fd58cc99c08ef0c406f23c
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-05-21 09:06:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['919539901834']
+global.prems = ['919539901834']
+global.allowed = ['919539901834']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -73,15 +73,15 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
-global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT'
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
+global.packname = '9ine.x3'
+global.author = '@false_eye'
+global.menuvid = 'https://telegra.ph/file/3bc3e04fdf6ce3143aedc.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/false_eye\n'
+global.dygp = 'https://t.me/sarca_stics'
+global.fgsc = 'https://github.com/false-eye'
+global.fgyt = 'https://t.me/false_eye'
+global.fgpyp = 'https://t.me/false_eye'
+global.fglog = 'https://telegra.ph/file/7202169e146c5ed2cb35f.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
