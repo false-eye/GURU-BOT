7bb660921c53f4d6e80d99359107c9add1b94889
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-05-21 08:19:40 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3'
-  let url = 'https://github.com/Guru322/GURU-BOT'
-  let murl = 'https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i'
-  let img = 'https://cdn.wallpapersafari.com/71/19/7ZfcpT.png'
+  var vn = 'https://cdn.jsdelivr.net/gh/false-eye/GURU-BOT@main/Assets/mp3/Alive.mp3'
+  let url = 'https://t.me/sarca_stics'
+  let murl = 'https://telegra.ph/file/d062f39327b707e27ea91.mp4'
+  let img = 'https://telegra.ph/file/81557b0cd9531411346f3.jpg'
   let con = {
     key: {
       fromMe: false,
@@ -25,15 +25,15 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     mimetype: 'audio/mpeg',
     ptt: true,
     waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: 'Guru',
+    fileName: '9x3',
 
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: 'I AM ALIVE',
-        body: 'GURU BOT',
+        title: 'Here I am Nigga',
+        body: '9x3 ft. 𝒇𝒂𝒍𝒔𝒆 𝒆𝒚𝒆',
         thumbnailUrl: img,
-        sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+        sourceUrl: 'https://instagram.com/false_eye',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
