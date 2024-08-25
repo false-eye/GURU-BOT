56c93fc4844f035e25ca5f267d11e7e378bbcc4c
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-08-25 20:46:35 +0530

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -18,7 +18,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     const result = await fetch(url)
     const tex = `
-⊱ ─── {* GURU FBDL*} ─── ⊰
+⊱ ─── {*FBDL*} ─── ⊰
 ↳ *VIDEO TITLE:* ${result.result.title}
 ⊱ ────── {⋆♬⋆} ────── ⊰`
 
