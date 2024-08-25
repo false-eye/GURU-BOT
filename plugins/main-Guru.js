eb309dcec91e2f5caee597c4ee473a35dc8ea8d1
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-08-25 20:55:35 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -2,29 +2,16 @@ let handler = async m =>
   m.reply(
     `
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+≡  9ine.x3 Created By @false_eye base by guru
 
 ─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
-
+▢ There ain't no supporting groups nigga 
 ─────────────
-≡ Disabled links? enter here! 
+▢ *Owner Instagram*
+ https://instagram.com/false_eye
 
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
-
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+▢ *Telegram*
+• https://t.me/false_eye
 
 
 `.trim()
