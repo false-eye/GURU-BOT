1c727136d48fb2f3d40f2621cdb2a73e31f46ce8
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-08-25 21:23:53 +0530

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -2,12 +2,12 @@ import { exec } from 'child_process'
 import speed from 'performance-now'
 
 let handler = async (m, { conn }) => {
-  let thumbnail = 'https://www.guruapi.tech/K.jpg'
+  let thumbnail = 'https://imgur.com/wADXTL5.jpg'
   let fgg = {
     key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
     message: {
       contactMessage: {
-        displayName: `GURU-BOT`,
+        displayName: `9x3 ~ 𝒇𝒂𝒍𝒔𝒆 𝒆𝒚𝒆`,
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'GURU-BOT'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
